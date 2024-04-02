0253f0322c8fd2d2dc27634bdfc8855f57d27062
Arata <newbluelight1031@gmail.com>
2024-04-02 22:49:28 +0900

    search user_id

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,21 @@
 class Post < ApplicationRecord
 
-belongs_to :user
-has_many :post_categories
-has_many :categories, through: :post_categories
+  belongs_to :user
+  has_many :post_categories
+  has_many :categories, through: :post_categories
+
+  def self.search(title,user_id)
+
+      if title.present? and user_id.present?
+          Post.where(['title LIKE ?', "%#{title}%"]).where(['user_id = ?', user_id])
+      elsif title.present? and user_id.blank?
+          Post.where(['title LIKE ?', "%#{title}%"])
+      elsif title.blank? and user_id.present?
+          Post.where(['user_id = ?', user_id])
+      else
+          Post.all
+      end
+
+  end
 
 end
